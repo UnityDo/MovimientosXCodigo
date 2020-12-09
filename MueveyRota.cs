0cf0d72fd178702448ee5ea39fc212436070f178
UnityDo <63395346+UnityDo@users.noreply.github.com>
2020-12-09 18:31:13 +0100

    Add files via upload

---
+++ MueveyRota.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MueveyRota : MonoBehaviour
+{
+    public float velocidad;
+    public float velocidadGiro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * velocidad,Space.World);
+      //  transform.Rotate(0, 0, velocidadGiro * Time.deltaTime);
+       // transform.Rotate(new Vector3(0, 0, 1),Time.deltaTime*velocidadGiro);
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * velocidadGiro), Space.Self);
+    }
+}
